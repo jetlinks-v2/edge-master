b032fd9909b9e728f4e4105be3ef977c6652b1eb
yangbo <yangbo_1@126.com>
2022-10-20 16:21:35 +0800

    添加边缘端、边端子设备接入网关

---
+++ src/main/java/org/jetlinks/pro/edge/gateway/EdgeChildDeviceGatewayProvider.java
@@ -0,0 +1,70 @@
+package org.jetlinks.pro.edge.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetlinks.core.message.codec.Transport;
+import org.jetlinks.pro.gateway.AbstractDeviceGateway;
+import org.jetlinks.pro.gateway.DeviceGateway;
+import org.jetlinks.pro.gateway.supports.DeviceGatewayProperties;
+import org.jetlinks.pro.gateway.supports.DeviceGatewayProvider;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+//边缘端子设备网关提供器，参看 ChildDeviceGatewayProvider
+@Slf4j
+@Component
+public class EdgeChildDeviceGatewayProvider implements DeviceGatewayProvider {
+
+    @Override
+    public String getId() {
+        return "edge-child-device";
+    }
+
+    @Override
+    public String getName() {
+        return "边缘端子设备接入";
+    }
+
+    @Override
+    public String getDescription() {
+        return "通过官方边缘端接入的子设备";
+    }
+
+    //接入方式：子设备
+    @Override
+    public String getChannel() {
+        return "edge-child-device";
+    }
+
+    /**
+     * 传输协议
+     * @return 返回传输协议
+     */
+    @Override
+    public Transport getTransport() {
+//        return DefaultTransport.MQTT;
+        return Transport.of("Gateway");
+    }
+
+    @Override
+    public Mono<? extends DeviceGateway> createDeviceGateway(DeviceGatewayProperties properties) {
+        return Mono.just(new EdgeChildDeviceGateway(properties.getId()));
+    }
+
+    //边缘端子设备网关
+    static class EdgeChildDeviceGateway extends AbstractDeviceGateway {
+
+        public EdgeChildDeviceGateway(String id) {
+            super(id);
+        }
+
+        @Override
+        protected Mono<Void> doShutdown() {
+            return Mono.empty();
+        }
+
+        @Override
+        protected Mono<Void> doStartup() {
+            return Mono.empty();
+        }
+    }
+}
---
+++ src/main/java/org/jetlinks/pro/edge/gateway/EdgeDeviceGateWayProvider.java
@@ -0,0 +1,95 @@
+package org.jetlinks.pro.edge.gateway;
+
+import org.jetlinks.core.ProtocolSupports;
+import org.jetlinks.core.device.DeviceRegistry;
+import org.jetlinks.core.device.session.DeviceSessionManager;
+import org.jetlinks.core.message.codec.DefaultTransport;
+import org.jetlinks.core.message.codec.Transport;
+import org.jetlinks.pro.gateway.DeviceGateway;
+import org.jetlinks.pro.gateway.supports.DeviceGatewayProperties;
+import org.jetlinks.pro.gateway.supports.DeviceGatewayProvider;
+import org.jetlinks.pro.network.DefaultNetworkType;
+import org.jetlinks.pro.network.NetworkManager;
+import org.jetlinks.pro.network.mqtt.gateway.device.MqttServerDeviceGateway;
+import org.jetlinks.pro.network.mqtt.server.MqttServer;
+import org.jetlinks.supports.server.DecodedClientMessageHandler;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+//参考 MqttServerDeviceGatewayProvider
+@Component
+public class EdgeDeviceGateWayProvider implements DeviceGatewayProvider {
+
+    //网络组件管理器，用于统一管理网络组件
+    private final NetworkManager networkManager;
+
+    //设备注册中心
+    private final DeviceRegistry registry;
+
+    //设备会话管理器
+    private final DeviceSessionManager sessionManager;
+
+    //客户端消息解码处理接口（解码后的设备消息处理器）
+    private final DecodedClientMessageHandler messageHandler;
+
+    //协议支持，EdgeProtocolSupport
+    private final ProtocolSupports protocolSupports;
+
+    public EdgeDeviceGateWayProvider(NetworkManager networkManager,
+                                     DeviceRegistry registry,
+                                     DeviceSessionManager sessionManager,
+                                     DecodedClientMessageHandler messageHandler,
+                                     ProtocolSupports protocolSupports) {
+        this.networkManager = networkManager;
+        this.registry = registry;
+        this.sessionManager = sessionManager;
+        this.messageHandler = messageHandler;
+        this.protocolSupports = protocolSupports;
+    }
+
+
+    /**
+     * 网关提供者id
+     * 同时也是消息协议id，参看 EdgeProtocolSupport
+     * @return
+     */
+    @Override
+    public String getId() {
+        return "official-edge-gateway";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getName() {
+        return "官方边缘端设备网关";
+    }
+
+    /**
+     * 传输协议
+     * @return
+     */
+    @Override
+    public Transport getTransport() {
+        return DefaultTransport.MQTT;
+    }
+
+    /**
+     * @param properties 配置
+     * @return
+     */
+    @Override
+    public Mono<? extends DeviceGateway> createDeviceGateway(DeviceGatewayProperties properties) {
+        return networkManager
+            .<MqttServer>getNetwork(DefaultNetworkType.MQTT_SERVER, properties.getChannelId()) //获取MqttServer网络组件
+            .map(mqttServer -> new MqttServerDeviceGateway(
+                properties.getId(),
+                registry,
+                sessionManager,
+                mqttServer,
+                messageHandler,
+                Mono.empty() //是否可以将EdgeProtocolSupport取代Mono.empty()应用于此
+            ));
+    }
+}
