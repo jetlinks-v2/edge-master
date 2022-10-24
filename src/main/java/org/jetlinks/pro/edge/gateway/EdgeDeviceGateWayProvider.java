519639c99f930a5a18a512c6d6551d83c8ea2e4d
yangbo <yangbo_1@126.com>
2022-10-24 09:19:48 +0800

    根据原型调整接入网关名称及描述

--- src/main/java/org/jetlinks/pro/edge/gateway/EdgeDeviceGateWayProvider.java
+++ src/main/java/org/jetlinks/pro/edge/gateway/EdgeDeviceGateWayProvider.java
@@ -63,7 +63,12 @@ public class EdgeDeviceGateWayProvider implements DeviceGatewayProvider {
      */
     @Override
     public String getName() {
-        return "官方边缘端设备网关";
+        return "边缘网关接入";
+    }
+
+    @Override
+    public String getDescription() {
+        return "适用于官方边缘网关设备接入";
     }
 
     /**
