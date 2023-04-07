71fa586614e57d67074ca29c1ace888581f135dc
zhangji <125540670@qq.com>
2023-04-07 17:45:51 +0800

    feat(网关设备): 解绑子设备后，禁用点位映射

--- src/main/java/org/jetlinks/pro/edge/protocol/EdgeProtocolSupport.java
+++ src/main/java/org/jetlinks/pro/edge/protocol/EdgeProtocolSupport.java
@@ -6,12 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetlinks.core.Embedded;
 import org.jetlinks.core.defaults.CompositeProtocolSupport;
 import org.jetlinks.core.device.DeviceFeatures;
+import org.jetlinks.core.device.DeviceOperator;
 import org.jetlinks.core.message.codec.DefaultTransport;
 import org.jetlinks.core.metadata.DefaultConfigMetadata;
 import org.jetlinks.core.metadata.DeviceConfigScope;
 import org.jetlinks.core.metadata.types.PasswordType;
 import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.pro.device.enums.DeviceFeature;
+import org.jetlinks.pro.edge.operations.RemoteEdgeOperations;
 import org.jetlinks.protocol.official.JetLinksAuthenticator;
 import org.jetlinks.protocol.official.JetLinksMqttDeviceMessageCodec;
 import org.jetlinks.protocol.official.TopicMessageCodec;
@@ -20,10 +22,13 @@ import org.jetlinks.supports.official.JetLinksDeviceMetadataCodec;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -31,6 +36,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class EdgeProtocolSupport extends CompositeProtocolSupport implements Embedded {
 
+    private final RemoteEdgeOperations operations;
 
     //边缘端消息协议id
     public static final String ID = "official-edge-protocol";
@@ -56,7 +62,8 @@ public class EdgeProtocolSupport extends CompositeProtocolSupport implements Emb
     }
 
     @SneakyThrows
-    public EdgeProtocolSupport() {
+    public EdgeProtocolSupport(RemoteEdgeOperations operations) {
+        this.operations = operations;
         setId(ID);
         setName(NAME);
         setMetadataCodec(new JetLinksDeviceMetadataCodec());
@@ -83,6 +90,25 @@ public class EdgeProtocolSupport extends CompositeProtocolSupport implements Emb
 
         // 子设备状态自管理
         addFeature(DeviceFeature.selfManageState);
+
+        doOnChildUnbind(this::disableMetadataMapping);
+    }
+
+    /**
+     * 处理网关子设备解绑事件
+     * <p>
+     * 禁用点位映射，停止上报采集数据
+     *
+     * @return Mono<Void>
+     */
+    public Mono<Void> disableMetadataMapping(DeviceOperator gateway,
+                                             Flux<DeviceOperator> child) {
+        return child
+            .map(DeviceOperator::getId)
+            .buffer(100)
+            .flatMap(deviceId -> operations
+                .invokeFunction(gateway.getId(), "device-collector-batch-disable", Collections.singletonMap("deviceId", deviceId)))
+            .then();
     }
 
 }
